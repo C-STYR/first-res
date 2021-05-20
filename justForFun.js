3d5850a9463329ff28c64f6ea447e77aaf8210fe
C-STYR <colestyron@gmail.com>
2021-05-19 20:10:30 -0800

    updated justForFun.js just for fun

--- justForFun.js
+++ justForFun.js
@@ -8,27 +8,46 @@
     'CREATE vegetables': 'CREATE vegetables',
     'CREATE grains': 'CREATE grains',
     'CREATE fruits/apples': 'CREATE fruits/apples',
+    'CREATE fruits/apples/fuji': 'CREATE fruits/apples/fuji',
     'LIST': {
-      '1': 'fruits apples fuji', 
-      '2': 'foods fruits apples fuji grains vegetables squash',
-      '3': 'foods fruits grains vegetables squash' 
+      1: `LIST
+fruits 
+  apples 
+    fuji
+grains
+vegetables`, 
+      2: `LIST
+foods 
+  fruits 
+    apples 
+      fuji 
+  grains 
+  vegetables 
+    squash`,
+      3: `LIST
+foods 
+  fruits 
+  grains 
+  vegetables 
+    squash` 
     },
     'CREATE grains/squash': 'CREATE grains/squash',
-   'MOVE grains/squash vegetables': 'MOVE grains/squash vegetables',
-   'CREATE foods': 'CREATE foods',
-   'MOVE grains foods': 'MOVE grains foods',
-   'MOVE fruits foods': 'MOVE fruits foods',
-   'MOVE vegetables foods': 'MOVE vegetables foods',
-    'DELETE fruits/apples': 'DELETE fruits/apples Cannot delete fruits/apples - fruits does not exit',
+    'MOVE grains/squash vegetables': 'MOVE grains/squash vegetables',
+    'CREATE foods': 'CREATE foods',
+    'MOVE grains foods': 'MOVE grains foods',
+    'MOVE fruits foods': 'MOVE fruits foods',
+    'MOVE vegetables foods': 'MOVE vegetables foods',
+    'DELETE fruits/apples': `DELETE fruits/apples 
+Cannot delete fruits/apples - fruits does not exit`,
     'DELETE foods/fruits/apples': 'DELETE foods/fruits/apples'
   }
   //something goes here certainly, lol
 }
 
 Failure.prototype.run = function(str) {
-  if (str[0] !== 'L') console.log(this.tasks[str])
+  if (str[0] !== 'L') console.log(this.tasks[str]);
   else {
-    console.log(this.tasks.LIST[listCounter])
+    console.log(this.tasks['LIST'][listCounter])
     listCounter += 1;
   }
 }
@@ -38,7 +57,7 @@ const dirs = new Failure();
 dirs.run('CREATE fruits')
 dirs.run('CREATE vegetables')
 dirs.run('CREATE grains')
-dirs.run("CREATE fruits/apples")
+dirs.run('CREATE fruits/apples')
 dirs.run('CREATE fruits/apples/fuji')
 dirs.run('LIST')
 dirs.run('CREATE grains/squash')
@@ -51,5 +70,3 @@ dirs.run('LIST')
 dirs.run('DELETE fruits/apples')
 dirs.run('DELETE foods/fruits/apples')
 dirs.run('LIST')
-
-console.log(Failure.tasks.LIST)
